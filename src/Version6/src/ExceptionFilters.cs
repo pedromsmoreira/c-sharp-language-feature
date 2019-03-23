7b426cd04cf43901c9e90c682792a23be4c9b11e
Pedro Moreira <pedromsmoreira51@gmail.com>
2019-03-23 21:07:04 +0000

    feat(version6): add exception filter example

---
+++ src/Version6/src/ExceptionFilters.cs
@@ -0,0 +1,50 @@
+﻿namespace Features.Version6
+{
+    using System;
+
+    public class ExceptionFilters
+    {
+        public void FilterExample(bool notFiltered)
+        {
+            try
+            {
+                this.ThrowCustomException(true);
+            }
+            // Exception Filters are clauses that determine when a given catch clause should be applied.
+            // If the expression used for an exception filter evaluates to true, the catch clause performs
+            // its normal processing on an exception.
+            // If the expression evaluates to false, then the catch clause is skipped.
+            catch (CustomException ce) when (ce.Message.Contains("Custom"))
+            {
+                Console.WriteLine($"Custom Exception Message.");
+            }
+            catch (CustomException ex)
+            {
+                Console.WriteLine($"Not Filtered Custom Exception :: {ex.Message}");
+            }
+        }
+
+        public void ThrowCustomException(bool @throw)
+        {
+            if (@throw)
+            {
+                throw new CustomException();
+            }
+
+            throw new CustomException("Do not filter");
+        }
+    }
+
+    public class CustomException : Exception
+    {
+        public CustomException()
+        : base("CustomExceptionMessage")
+        {
+        }
+
+        public CustomException(string message)
+        : base(message)
+        {
+        }
+    }
+}
