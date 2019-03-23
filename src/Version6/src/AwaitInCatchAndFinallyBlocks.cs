b56ea364e6eaf0e0ad3175a73d56d0c9205e26b8
Pedro Moreira <pedromsmoreira51@gmail.com>
2019-03-23 21:20:28 +0000

    feat(version6): add await in catch and finally block example

---
+++ src/Version6/src/AwaitInCatchAndFinallyBlocks.cs
@@ -0,0 +1,30 @@
+﻿namespace Features.Version6
+{
+    using System;
+    using System.Threading.Tasks;
+
+    // C# 5 had several limitations around where you could place await expressions.
+    // With C# 6, you can now use await in catch or finally expressions.
+    public class AwaitInCatchAndFinallyBlocks
+    {
+        public async Task AwaitInCatchAndFinally()
+        {
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                await this.ReturnSomeTask();
+            }
+            finally
+            {
+                await this.ReturnSomeTask();
+            }
+        }
+
+        public Task ReturnSomeTask()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
