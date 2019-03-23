d5ce6519901418b9015ce76e0a3fa8849424ca85
Pedro Moreira <pedromsmoreira51@gmail.com>
2019-03-23 14:06:52 +0000

    feat(version6): add auto-property initializers example

---
+++ src/Version6/AutoPropertyInitializers.cs
@@ -0,0 +1,10 @@
+﻿namespace Features.Version6
+{
+    using System.Collections.Generic;
+
+    public class AutoPropertyInitializers
+    {
+        // Auto-property initializers let you declare the initial value for an auto-property as part of the property declaration.
+        public IDictionary<string, object> ObjectDict { get; } = new Dictionary<string, object>();
+    }
+}
