161acab35a234a905ef81bf357a8c27820d71e42
Pedro Moreira <pedromsmoreira51@gmail.com>
2019-03-23 14:44:33 +0000

    feat(version6): add using static example

---
+++ src/Version6/UsingStatic.cs
@@ -0,0 +1,10 @@
+﻿namespace Features.Version6
+{
+    // The using static enhancement enables you to import the static methods of a single class.
+    using static System.Math;
+
+    public class UsingStatic
+    {
+        public double Power(int x, int y) => Pow(x, y);
+    }
+}
