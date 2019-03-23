e2137652fcb5e87513eac6eb7559c4049a6a98bd
Pedro Moreira <pedromsmoreira51@gmail.com>
2019-03-23 14:56:32 +0000

    feat(version6): add null conditional operator example

---
+++ src/Version6/src/Version6/NullConditionalOperator.cs
@@ -0,0 +1,19 @@
+﻿namespace Features.Version6
+{
+    // The rules of the ?. operator ensure that the left-hand side of the operator is evaluated only once.
+    public class NullConditionalOperator
+    {
+        // used = null to force a compiler check and ask for nullconditional operator
+        public void Print(Person person = null)
+        {
+            // use this
+            var first = person?.FirstName;
+
+            // instead of
+            if (person != null)
+            {
+                var first2 = person.FirstName;
+            }
+        }
+    }
+}
---
+++ src/Version6/src/Version6/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace Features.Version6
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
