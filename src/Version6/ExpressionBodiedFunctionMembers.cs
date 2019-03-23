b2e217f3dcede66955b7ba65b4a3f966a16420db
Pedro Moreira <pedromsmoreira51@gmail.com>
2019-03-23 14:18:08 +0000

    feat(version6): add expression bodied function members example

---
+++ src/Version6/ExpressionBodiedFunctionMembers.cs
@@ -0,0 +1,15 @@
+﻿namespace Features.Version6
+{
+    public class ExpressionBodiedFunctionMembers
+    {
+        // Many members that you write are single statements that could be single expressions.
+        // Write an expression-bodied member instead. It works for methods and read-only properties.
+        public int Multiply(int n, int multiplier) => n * multiplier;
+
+        public string FirstName { get; set; } = "Hello";
+
+        public string LastName { get; set; } = "Dummy";
+
+        public string FullName => $"{this.FirstName} {this.LastName}";
+    }
+}
